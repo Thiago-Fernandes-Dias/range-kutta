5fbdbd3102f056f971c31578b50e59da48dda1b6
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-12-02 22:27:37 -0300

    - Refatoração; - Criação de gráfico com h=0.25

--- solution.py
+++ solution.py
@@ -24,24 +24,31 @@ def sum_digits(num: int) -> int:
 
 def average(values: list[float]) -> float:
     return sum(values) / len(values)
+
+def generate_table(points: tuple[list[float], list[float]]) -> str:
+   return tabulate(list(zip(range(0,table_rows + 1), points[0][:table_rows + 1], 
+                        points[1][:table_rows + 1])), 
+               headers=["Iteração", "Tempo [s]", "Temperatura [°C]"], 
+               tablefmt="fancy_grid")
     
+K = 0.25
+
 initial_temp = round(sum_digits(11202130847) * 1.5)
 print(f"Temperatura especificada: {initial_temp}°C")
 
-fig, ax = plt.subplots()
-ax.set_xlabel("Tempo [s]")
-ax.set_ylabel("Temperatura [°C]")
-ax.autoscale()
+_, ax_h1 = plt.subplots()
+ax_h1.set_xlabel("Tempo [s]")
+ax_h1.set_ylabel("Temperatura [°C]")
+ax_h1.autoscale()
 
-K = 0.25
-points = range_kutta(90, 0, 0.1, 10000, lambda _, y: -K * (y - 20)) 
+points_h1 = range_kutta(90, 0, 0.1, 10000, lambda _, y: -K * (y - 20)) 
 
 avg_timestamp, avg_temp = 0, 0
-for i in range(0, len(points[0])):
-    if points[1][i] <= initial_temp:
-        avg_timestamp = average(points[0][i - 1:i + 1])
-        avg_temp = average(points[1][i - 1:i + 1])
-        ax.scatter(avg_timestamp, avg_temp, color='red')
+for i in range(0, len(points_h1[0])):
+    if points_h1[1][i] <= initial_temp:
+        avg_timestamp = average(points_h1[0][i - 1:i + 1])
+        avg_temp = average(points_h1[1][i - 1:i + 1])
+        ax_h1.scatter(avg_timestamp, avg_temp, color='red')
         # Não é necessário verificar o tamanho do array para acessar o valor
         # da posição i + 1, pois a temperatura final é menor do que a 
         # especificada.
@@ -50,10 +57,22 @@ for i in range(0, len(points[0])):
 table_rows = 5
 print(f"Tempo de resfriamento aproximado: {avg_timestamp:.2f}s")
 print(f"Temperatura após o tempo de resfriamento aproximado: {avg_temp:.2f}°C")
-print("Tabela:")        
-print(tabulate(list(zip(range(0,table_rows + 1), points[0][:table_rows + 1], 
-                        points[1][:table_rows + 1])), 
-               headers=["Iteração", "Tempo [s]", "Temperatura [°C]"], 
-               tablefmt="fancy_grid"))
-ax.plot(points[0], points[1], scalex=False, scaley=False)
-plt.savefig("graph.png")
+print("Tabela (h=0.1s):")        
+print(generate_table(points_h1))
+
+ax_h1.plot(points_h1[0], points_h1[1], scalex=False, scaley=False)
+plt.savefig("graph-h1.png")
+
+_, ax_h2 = plt.subplots()
+ax_h2.set_xlabel("Tempo [s]")
+ax_h2.set_ylabel("Temperatura [°C]")
+ax_h2.autoscale()
+
+points_h2 = range_kutta(90, 0, 0.25, 10000, lambda _, y: -K * (y - 20)) 
+
+table_rows = 5
+print("Tabela (h=0.25s):")        
+print(generate_table(points_h2))
+ax_h2.plot(points_h2[0], points_h2[1], scalex=False, scaley=False)
+plt.savefig("graph-h2.png")
+
